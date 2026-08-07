9a1232e01a9d0544f86aeea2ff008c5ce2488ab8
JosipM <josip10mikulic@gmail.com>
2026-08-07 21:54:25 +0200

    Fix project structure

--- src/src/main/java/com/example/blackjack/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.example.blackjack;
-
-public class Launcher {
-    public static void main(String[] args) {
-        BlackJackApplication.main(args);
-    }
-}
-
--- src/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.blackjack {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.controlsfx.controls;
-
-    opens com.example.blackjack to javafx.fxml;
-    exports com.example.blackjack;
-}
